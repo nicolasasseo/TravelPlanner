7c910897aebf950e6f3086bdfc9dfccb9aea5b18
nicolasasseo <n.asseo23@ejm.org>
2025-06-25 15:03:30 +0200

    Set up database using Postgres, set up authentication with GitHub OAuth, started working on adding trips functionality

---
+++ app/trips/page.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@/auth"
+import React from "react"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+const page = async () => {
+  const session = await auth()
+  if (!session) {
+    return (
+      <div className="flex justify-center items-center h-screen text-gray-700 text-xl">
+        Please Sign In.
+      </div>
+    )
+  }
+  return (
+    <div className="space-y-6 container mx-auto px-4 py-8">
+      <div>
+        <h1>Dashboard</h1>
+        <Link href="/trips/new">
+          <Button>New Trip</Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ lib/actions/create-trip.ts
@@ -0,0 +1,35 @@
+import { auth } from "@/auth"
+import { prisma } from "@/lib/prisma"
+import { redirect } from "next/navigation"
+
+const createTrip = async (formData: FormData) => {
+  const session = await auth()
+  if (!session || !session.user?.id) {
+    throw new Error("Not authenticated")
+  }
+  const title = formData.get("title")?.toString()
+  const description = formData.get("description")?.toString()
+  const startDateStr = formData.get("startDate")?.toString()
+  const endDateStr = formData.get("endDate")?.toString()
+
+  if (!title || !description || !startDateStr || !endDateStr) {
+    throw new Error("All fields are required")
+  }
+
+  const startDate = new Date(startDateStr)
+  const endDate = new Date(endDateStr)
+
+  await prisma.trip.create({
+    data: {
+      title,
+      description,
+      startDate,
+      endDate,
+      userId: session.user.id,
+    },
+  })
+
+  redirect("/trips")
+}
+
+export default createTrip
